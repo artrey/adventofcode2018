13a7dc276c446eaaa2f5f79de9910e7c758c52a7
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2018-12-13 01:01:55 +0300

    Fixed geometry package

--- utils/geometry/point.py
+++ utils/geometry/point.py
@@ -3,8 +3,5 @@ class Point:
         self.x = x
         self.y = y
 
-    def manhattan_length(self, x: int, y: int) -> int:
-        return abs(self.x - x) + abs(self.y - y)
-
     def __repr__(self) -> str:
         return f'({self.x}, {self.y})'
