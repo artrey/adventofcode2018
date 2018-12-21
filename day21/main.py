0ac936e012cdf97a541e90894400144e4c5ff4ea
Alexander <oz.sasha.ivanov@gmail.com>
2018-12-21 12:19:42 +0300

    Improved day 21

--- day21/main.py
+++ day21/main.py
@@ -2,49 +2,45 @@ import typing
 
 
 def compiled_alg() -> int:
-    r = [0] * 6
-    r[4] = 65536
-    r[3] = 10649702
+    r4 = 65536
+    r3 = 10649702
 
     while True:
-        r[5] = r[4] & 255
-        r[3] += r[5]
-        r[3] &= 16777215
-        r[3] *= 65899
-        r[3] &= 16777215
+        r3 += r4 & 255
+        r3 &= 16777215
+        r3 *= 65899
+        r3 &= 16777215
 
-        if r[4] < 256:
-            return r[3]
+        if r4 < 256:
+            return r3
 
-        r[4] //= 256
+        r4 //= 256
 
 
 def compiled_alg_max() -> int:
     store: typing.Set[int] = set()
     last_value = 0
 
-    r = [0] * 6
-    r[4] = 65536
-    r[3] = 10649702
+    r4 = 65536
+    r3 = 10649702
 
     while True:
-        r[5] = r[4] & 255
-        r[3] += r[5]
-        r[3] &= 16777215
-        r[3] *= 65899
-        r[3] &= 16777215
+        r3 += r4 & 255
+        r3 &= 16777215
+        r3 *= 65899
+        r3 &= 16777215
 
-        if r[4] < 256:
-            if r[3] not in store:
-                last_value = r[3]
+        if r4 < 256:
+            if r3 not in store:
+                last_value = r3
             else:
                 return last_value
-            store.add(r[3])
-            r[4] = r[3] | 65536
-            r[3] = 10649702
+            store.add(r3)
+            r4 = r3 | 65536
+            r3 = 10649702
             continue
 
-        r[4] //= 256
+        r4 //= 256
 
 
 if __name__ == '__main__':
